f39c469ba52e59cb4eeadd39e0beae46c61509ba
BadComoc <bad1comic@gmail.com>
2023-06-18 10:49:15 +0930

    CDDS Task 2 Included Raylib & Raygui

--- Doubly-Linked-List/src/main.cpp
+++ Doubly-Linked-List/src/main.cpp
@@ -1,9 +1,26 @@
-#include <iostream>
+#include <raylib.h>
+
+#define RAYGUI_IMPLEMENTATION
+#include <raygui.h>
 
 int main()
 {
-	std::cout << "Hello, World" << std::endl;
-	std::cin.ignore();
+	InitWindow(800, 600, "Doubly Linked List");
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+
+		DrawText("Window", 190, 200, 20, LIGHTGRAY);
+
+		GuiButton({ 600, 40, 120, 20 }, "Test");
+
+		EndDrawing();
+	}
+
+	CloseWindow();
 
 	return 0;
 }
